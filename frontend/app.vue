53ca90ce7bd4a0e357e60bcaa248e2dfc0c93efe
Siddharth Khattar <siddharth.khattar@threedy.io>
2024-10-19 00:35:09 +0200

    Add: Morphing Gradient background

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,16 @@
+<script lang="ts" setup>
+import MorphingGradient from "@/components/MorphingGradient.vue";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="w-screen h-screen bg-neutral-950">
+    <MorphingGradient
+      :firstColor="'27, 0, 96'"
+      :secondColor="'246, 97, 88'"
+      :thirdColor="'100, 220, 255'"
+      :fourthColor="'160, 0, 102'"
+      :fifthColor="'76, 19, 129'"
+      :interactiveColor="'239, 180, 230'"
+    />
   </div>
 </template>
