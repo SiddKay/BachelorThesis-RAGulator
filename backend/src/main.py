7390871637c495377c6df50fa54f63391fba542f
SiddKayy <siddharthkhattar0058@protonmail.com>
2024-11-08 12:57:44 +0100

    Add: Custom logging

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,29 @@
-import uvicorn
-from .app.server import app
+# import uvicorn
+from services.logger import LoggingManager, get_logger
+
+# from .app.server import app
+
+logger = get_logger("ragulator_logger")
+
+
+def main():
+    # Setup logging
+    logging_manager = LoggingManager()
+    logging_manager.setup()
+
+    # Basic code to test logging
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.exception("exception message")
+    logger.info("Starting Uvicorn server...")
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    main()
+    # uvicorn.run(app, host="localhost", port=8000)
